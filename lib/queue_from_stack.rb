2bbcbd4de4bb02d4205c2dc4a93c21ca512d0be6
Tasreen Mowreen <tmowreen97@gmail.com>
2023-02-26 19:45:09 -0500

    working on it

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,46 @@
 require_relative './stack'
+require 'pry'
 
 # your code here
+class MyQueue
+  def initialize
+    @s1 = Stack.new
+    @s2 = Stack.new
+  end
+
+  def add value
+    @s1.push(value)
+    @s2.push(value)
+  end
+
+  def remove
+    # binding.pry
+    result = []
+    element = @s1.peek
+    until @s1.empty?
+      if @s1.peek
+        result.push(@s1.peek)
+        element = @s1.peek
+        @s2.pop
+
+        @s1.pop
+      else
+        element
+      end
+    end
+    @s1.push(result).flatten!.pop
+    @s2.push(result).flatten!.pop
+    element
+    # binding.pry
+   end
+
+  def peek
+    
+
+
+  end
+
+
+
+
+end
